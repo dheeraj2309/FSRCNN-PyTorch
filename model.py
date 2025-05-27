0b7c4e5a248a52d7eb930b366301fd8c7f92d414
Dheeraj <dheerajw2309@gmail.com>
2025-05-27 20:26:13 +0530

    try

--- model.py
+++ model.py
@@ -50,12 +50,6 @@ class FSRCNN(nn.Module):
             nn.Conv2d(12, 12, (3, 3), (1, 1), (1, 1)),
             nn.PReLU(12),
             nn.Conv2d(12, 12, (3, 3), (1, 1), (1, 1)),
-            nn.PReLU(12),
-            nn.Conv2d(12, 12, (3, 3), (1, 1), (1, 1)),
-            nn.PReLU(12),
-            nn.Conv2d(12, 12, (3, 3), (1, 1), (1, 1)),
-            nn.PReLU(12),
-            nn.Conv2d(12, 12, (3, 3), (1, 1), (1, 1)),
             nn.PReLU(12)
         )
 
